d1afc12c8dd1332b9088d6eeec8160f5d74c7412
Pascal Jufer <pascal-jufer@bluewin.ch>
2025-02-24 14:21:16 +0100

    Fix lint issues

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,8 +1,10 @@
+import type { Config } from './lib/get-config.js';
+import type { Issue } from './lib/get-stale-issues.js';
 import core from '@actions/core';
 import { expect, it, vi } from 'vitest';
 import { closeIssue } from './lib/close-issue.js';
-import { type Config, getConfig } from './lib/get-config.js';
-import { getStaleIssues, type Issue } from './lib/get-stale-issues.js';
+import { getConfig } from './lib/get-config.js';
+import { getStaleIssues } from './lib/get-stale-issues.js';
 import { main } from './main.js';
 
 vi.mock('./lib/get-config.js');
